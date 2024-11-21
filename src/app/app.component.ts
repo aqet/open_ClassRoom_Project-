4762f66d68b5bb8ffd143fe8d484c2b839589df2
aqet <tientcheuigorcarel@mail.com>
2024-11-21 18:22:27 +0100

    yes  les problemes asyn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { Observable, interval } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [
+    // HeaderComponent, // Décommentez si HeaderComponent est utilisé
+    // RouterOutlet,    // Décommentez si RouterOutlet est utilisé
+    AsyncPipe         // Permet d'utiliser l'AsyncPipe dans le template
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'] // Corrigé en "styleUrls"
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
+  interval$!: Observable<number>; // Observable qui émet une valeur chaque seconde
+
+  ngOnInit() {
+    this.interval$ = interval(1000); // Observable qui émet toutes les secondes
+  }
 }
